adfd6660ba893a443b3b0c2add54d58483c881f7
이상호 <kigkm@naver.com>
2021-04-20 20:32:40 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import AppLayout from '../components/AppLayout';
+import React from 'react';
+
+const Home = () => {
+  return (
+    <AppLayout>
+      <div>hello, next</div>
+    </AppLayout>
+  );
+};
+
+export default Home;
