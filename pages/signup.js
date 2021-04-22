09237972302a8ed156de54a0c9c893f58752ef95
Lee sang ho <qapl8165@gmail.com>
2021-04-22 20:09:10 +0900

    second commit

--- pages/signup.js
+++ pages/signup.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import AppLayout from '../components/AppLayout';
-
+import Head from 'next/head'
 const Signup = () => {
-  return <AppLayout>회원가입 페이지</AppLayout>;
+  return(
+    <>
+      <Head>
+        <title>회원 가입</title>
+      </Head>
+    <AppLayout>회원가입 페이지</AppLayout>;
+    </>
+  ) 
 };
 
 export default Signup;
